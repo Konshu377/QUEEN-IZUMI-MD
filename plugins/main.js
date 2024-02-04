8b2d6bcc63307cb6ae0e0c0d72bebffd24fb8816
VajiraTech <158836060+VajiraTech@users.noreply.github.com>
2024-02-04 23:25:57 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - V1 🧚

--- plugins/main.js
+++ plugins/main.js
@@ -2,9 +2,10 @@ const config = require('../config')
 const os = require('os')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
 cmd({
     pattern: "alive",
-    react: "🍬",
+    react: "👨‍💻",
     alias: ["online","test","bot"],
     desc: "Check bot online or no.",
     category: "main",
@@ -22,20 +23,20 @@ let monspacenew ='`'
 if(config.ALIVE === "default") {
 const buttons = [
   {buttonId: prefix + 'menu' , buttonText: {displayText: 'COMMANDS MENU'}, type: 1},
-  {buttonId: prefix + 'ping' , buttonText: {displayText: 'BOT\'S SPEED'}, type: 1}
+  {buttonId: prefix + 'ping' , buttonText: {displayText: 'QUEEN-IZUMI-MD SPEED'}, type: 1}
 ]
 const buttonMessage = {
     image: {url: config.LOGO},
     caption: `${monspace}👋 Hello ${pushname} I'm alive now${monspace}
+    
+*🚀Version:* ${require("../package.json").version}
+*⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🕒Runtime:* ${runtime(process.uptime())}
+*📍Platform:* ${hostname}
 
-*👾 Im QUEEN-IZUMI-MD whatsapp bot*
-    
-> *Version:* ${require("../package.json").version}
-> *Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-> *Runtime:* ${runtime(process.uptime())}
-> *Platform:* ${hostname}
-    
-*🍭 Have A Nice Day 🍭*`,
+🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
+                    
+*🌻Have A Nice Day..*🌻`,
     footer: config.FOOTER,
     buttons: buttons,
     headerType: 4
@@ -83,7 +84,7 @@ l(e)
 
 cmd({
   pattern: "menu",
-  react: "🗃️",
+  react: "👨‍💻",
   alias: ["panel","list","commands"],
   desc: "Get bot\'s command list.",
   category: "main",
@@ -98,24 +99,24 @@ else if(os.hostname().length == 8) hostname = 'koyeb'
 else hostname = os.hostname()
 let monspace ='```'
 const buttons = [
-{buttonId: prefix + 'downmenu' , buttonText: {displayText: 'DOWNLOAD COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'searchmenu' , buttonText: {displayText: 'SEARCH COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'convertmenu' , buttonText: {displayText: 'CONVERT COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'logomenu' , buttonText: {displayText: 'LOGO COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'othermenu' , buttonText: {displayText: 'OTHER COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'ownermenu' , buttonText: {displayText: 'OWNER COMMANDS MENU'}, type: 1},
-{buttonId: prefix + 'adminmenu' , buttonText: {displayText: 'ADMIN COMMANDS MENU'}, type: 1}
+{buttonId: prefix + 'downmenu' , buttonText: {displayText: 'ᴅᴏᴡɴʟᴏᴀᴅ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'searchmenu' , buttonText: {displayText: 'ꜱᴇᴀʀᴄʜ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'convertmenu' , buttonText: {displayText: 'ᴄᴏɴᴠᴇʀᴛ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'logomenu' , buttonText: {displayText: 'ʟᴏɢᴏ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'ownermenu' , buttonText: {displayText: 'ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'adminmenu' , buttonText: {displayText: 'ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1},
+{buttonId: prefix + 'othermenu' , buttonText: {displayText: 'ᴏᴛʜᴇʀ ᴄᴏᴍᴍᴀɴᴅꜱ'}, type: 1}
 ]
 const buttonMessage = {
   image: {url: config.LOGO},
   caption: `${monspace}👋 Hello ${pushname}${monspace}
 
-*👾 Zero-Two MD commands menu...*
+*👾 QUEEN-IZUMI-MD commands menu...*
   
-> *Version:* ${require("../package.json").version}
-> *Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-> *Runtime:* ${runtime(process.uptime())}
-> *Platform:* ${hostname}`,
+ *🚀Version:* ${require("../package.json").version}
+ *⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+ *🕒Runtime:* ${runtime(process.uptime())}
+ *📍Platform:* ${hostname}`,
   footer: config.FOOTER,
   buttons: buttons,
   headerType: 4
