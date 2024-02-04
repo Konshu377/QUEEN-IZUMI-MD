371e82ae705b50ee9f8b15a8f0e61db956945084
VajiraTech <158836060+VajiraTech@users.noreply.github.com>
2024-02-04 21:34:04 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - V1 🧚

--- config.js
+++ config.js
@@ -6,5 +6,16 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'ZEROTWO=kmsRFKBD#CSMW_N13TIMTA3i5fUHIoXqX7XGrNUKCgxtz34QmI2M'
+SESSION_ID: process.env.SESSION_ID || 'youre session id',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
+LANG: process.env.BOT_LANG || 'EN' ,
+PREFIX: process.env.PREFIX || '.',
+ANTI_BAD: process.env.ANTI_BAD || 'false',
+MAX_SIZE: process.env.MAX_SIZE || 300,
+ONLY_GROUP: process.env.ONLY_GROUP || 'false',
+ANTI_LINK: process.env.ANTI_LINK || 'false' ,
+ANTI_BOT: process.env.ANTI_BOT || 'false',
+ALIVE: process.env.ALIVE || `default`,
+FOOTER: process.env.FOOTER ||  '©ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ',
+LOGO: process.env.LOGO || `https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg` 
 };
