5b4ab3a2ac32e4adee1e41c72d7ff25014060332
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-05 21:29:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -516,11 +516,11 @@ if (!body.includes('http')) {
   }
 }
 //====================================================================
-/*const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/raw'
+const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/raw'
 let { data } = await axios.get(url)
 for (vr in data){
 if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
-}*/
+}
 var check_id = ((id) => {
   var data = {
     is_bot: false,
