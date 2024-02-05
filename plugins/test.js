ca54b07b16446e04b791c915255e9c48acb8cdb9
Thisal Sanujaya <90607090+sanuwaofficial@users.noreply.github.com>
2024-02-05 19:59:03 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -32,31 +32,7 @@ if(config.ALIVE === "default") {
     } 
 ]
 const listMessage = {
-  caption: `*QUEEN IZUMI WHATSAPP USER BOT* 💫
-
-                     *OUR MISSION*
-
-🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
-
-
-🐼 The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-
-
-💡 Various things can be downloaded from this bot.  Also, managing groups, making logos & edit-images in different ways, searching for different things and getting information and more futures included.
-
-
-⚠️ Also, if your Whatsapp account gets damaged or banned by using this, we are not responsible and you should take responsibility for it.
-
-
-👨‍💻 OWNER VAJIRA AND TIMASHA
-
-🎡 *GITHUB:*  C O M I N G  S O O N
-
-🪩 *OUR GROUP:* https://chat.whatsapp.com/D6w6Qy5yrhp7MmfNcprbO3
-
-*ᴘʟᴇᴀꜱᴇ ꜱᴛᴀʀ ᴛʜᴇ ʀᴇᴘᴏ ᴀɴᴅ ꜰᴏʟʟᴏᴡ ᴍᴇ ᴏɴ ɢɪᴛʜᴜʙ* 
-
-*C O M I N G  S O O N*`,
+  caption: `test`,
   image : { url : config.LOGO} ,
   footer: config.FOOTER,
   buttonText: "🔢 Reply below number,",
