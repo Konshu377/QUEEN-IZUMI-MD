1ed6c95a3a51421e58441b2b13b5262e5d1adf91
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-06 06:41:07 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - V1 🧚

--- plugins/main.js
+++ plugins/main.js
@@ -3,6 +3,7 @@ const os = require('os')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
+
 cmd({
     pattern: "alive",
     react: "👨‍💻",
@@ -14,6 +15,7 @@ cmd({
 },
 async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+	var msg = mek
 if(os.hostname().length == 12 ) hostname = 'replit'
 else if(os.hostname().length == 36) hostname = 'heroku'
 else if(os.hostname().length == 8) hostname = 'koyeb'
@@ -21,13 +23,18 @@ else hostname = os.hostname()
 let monspace ='```'
 let monspacenew ='`'
 if(config.ALIVE === "default") {
-const buttons = [
-  {buttonId: prefix + 'menu' , buttonText: {displayText: 'COMMANDS MENU'}, type: 1},
-  {buttonId: prefix + 'ping' , buttonText: {displayText: 'QUEEN-IZUMI-MD SPEED'}, type: 1}
+ const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'menu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'ping' , description: 'QUEEN-IZUMI-MD SPEED'},
+
+	]
+    } 
 ]
-const buttonMessage = {
-    image: {url: config.LOGO},
-    caption: `${monspace}👋 Hello ${pushname} I'm alive now${monspace}
+const listMessage = {
+  caption: `${monspace}👋 Hello ${pushname} I'm alive now${monspace}
     
 *🚀Version:* ${require("../package.json").version}
 *⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
@@ -37,29 +44,64 @@ const buttonMessage = {
 🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
                     
 *🌻Have A Nice Day..*🌻`,
-    footer: config.FOOTER,
-    buttons: buttons,
-    headerType: 4
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: '🥽 𝗔𝗤𝗨𝗔𝗕𝗢𝗧 𝗠𝗗 V2💦',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://telegra.ph/file/85fe740b2385a55178500.jpg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
 }
-return await conn.buttonMessage2(from, buttonMessage)}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg }) 
+}
 else {
-  const buttons = [
-    {buttonId: prefix + 'menu' , buttonText: {displayText: 'COMMANDS MENU'}, type: 1},
-    {buttonId: prefix + 'ping' , buttonText: {displayText: 'BOT\'S SPEED'}, type: 1}
-  ]
-  const buttonMessage = {
-      image: {url: config.LOGO},
-      caption: config.ALIVE,
-      footer: config.FOOTER,
-      buttons: buttons,
-      headerType: 4
-  }
-  return await conn.buttonMessage2(from, buttonMessage, mek)}
+  const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'menu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'ping' , description: 'QUEEN-IZUMI-MD SPEED'} ,
+
+	]
+    } 
+]
+const listMessage = {
+  caption: config.ALIVE,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: '🥽 𝗔𝗤𝗨𝗔𝗕𝗢𝗧 𝗠𝗗 V2💦',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://telegra.ph/file/85fe740b2385a55178500.jpg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg })
+}
 } catch (e) {
 reply('*Error !!*')
 l(e)
 }
 })
+
+
 
 cmd({
     pattern: "ping",
