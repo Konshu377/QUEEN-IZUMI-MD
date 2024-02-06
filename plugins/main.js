630ea55048535a1719b2cf79efd2068f0fabdffb
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-06 06:47:54 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - V1 🧚

--- plugins/main.js
+++ plugins/main.js
@@ -169,3 +169,114 @@ reply('*Error !!*')
 l(e)
 }
 })
+
+
+cmd({
+    pattern: "alive",
+    react: "👨‍💻",
+    alias: ["online","test","bot"],
+    desc: "Check bot online or no.",
+    category: "main",
+    use: '.alive',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+	var msg = mek
+if(os.hostname().length == 12 ) hostname = 'replit'
+else if(os.hostname().length == 36) hostname = 'heroku'
+else if(os.hostname().length == 8) hostname = 'koyeb'
+else hostname = os.hostname()
+let monspace ='```'
+let monspacenew ='`'
+if(config.ALIVE === "default") {
+ const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'downmenu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'searchmenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'convertmenu' , description: 'QUEEN-IZUMI-MD SPEED'}, 
+	    {title: "2", rowId: prefix + 'logomenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'ownermenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'adminmenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'othermenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+
+	]
+    } 
+]
+const listMessage = {
+  caption: `${monspace}👋 Hello ${pushname} I'm alive now${monspace}
+    
+*🚀Version:* ${require("../package.json").version}
+*⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🕒Runtime:* ${runtime(process.uptime())}
+*📍Platform:* ${hostname}
+
+🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
+                    
+*🌻Have A Nice Day..*🌻`,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: '🥽 𝗔𝗤𝗨𝗔𝗕𝗢𝗧 𝗠𝗗 V2💦',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://telegra.ph/file/85fe740b2385a55178500.jpg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg }) 
+}
+else {
+  const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'downmenu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'searchmenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'convertmenu' , description: 'QUEEN-IZUMI-MD SPEED'}, 
+	    {title: "2", rowId: prefix + 'logomenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'ownermenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'adminmenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+	    {title: "2", rowId: prefix + 'othermenu' , description: 'QUEEN-IZUMI-MD SPEED'},
+
+
+	]
+    } 
+]
+const listMessage = {
+  caption: config.ALIVE,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: '🥽 𝗔𝗤𝗨𝗔𝗕𝗢𝗧 𝗠𝗗 V2💦',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://telegra.ph/file/85fe740b2385a55178500.jpg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg })
+}
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+	
