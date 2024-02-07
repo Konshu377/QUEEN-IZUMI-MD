db74a7f1d88bdd34203c49ea3c26f4bcbb6a0412
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-07 13:31:38 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚

--- plugins/soundcloud.js
+++ plugins/soundcloud.js
@@ -64,8 +64,9 @@ var srh = [];
 for (var i = 0; i < data.length; i++) {
   if(data[i].thumb && !data[i].views.includes('Follow')){
 srh.push({
-title: data[i].title,
+description: data[i].title,
 description: data[i].artist + ' | ' + data[i].views + ' | '+ data[i].release + ' | '+ data[i].timestamp,
+title: i + 1,
 rowId: prefix + 'selectaud2 ' + data[i].url
 });
   }
