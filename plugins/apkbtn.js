b699fa3c55c5416f569b794ee4fc8df1f924cde9
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-07 20:33:17 +0530

    Update apkbtn.js

--- plugins/apkbtn.js
+++ plugins/apkbtn.js
@@ -1,112 +1,45 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
-const gis = require('async-g-i-s');
-const {unsplash, pixabay} = require("@sl-code-lords/image-library")
+var os = require('os')
 
-var imgmsg =''
-if(config.LANG === 'SI') imgmsg = "```කරුණාකර වචන කිහිපයක් ලියන්න!```"
-else imgmsg = "```Please write a few words!```"
-
-var desc =''
-if(config.LANG === 'SI') desc = "ගූගල් හි අදාළ පින්තූර සෙවීම."
-else desc = "Search for related pics on Google."
-
-var desc2 =''
-if(config.LANG === 'SI') desc2 = "unsplash.com හි අදාළ පින්තූර සෙවීම."
-else desc2 = "Search for related pics on unsplash.com."
-
-var desc3 =''
-if(config.LANG === 'SI') desc3 = "pixabay.com හි අදාළ පින්තූර සෙවීම."
-else desc3 = "Search for related pics on pixabay.com."
-
-var desc4 =''
-if(config.LANG === 'SI') desc4 = "bing හි අදාළ පින්තූර සෙවීම."
-else desc4 = "Searche for related pics on bing."
-
-var errt =''
-if(config.LANG === 'SI') errt = "*මට කිසිවක් සොයාගත නොහැකි විය :(*"
-else errt = "*I couldn't find anything :(*"
+var tmsg =''
+if(config.LANG === 'SI') tmsg = 'එය Bot link ලබා දෙයි.'
+else tmsg = "It gives bot link."
 
 
 cmd({
     pattern: "apk",
-    react: '🖼️',
-    desc: desc2,
-    category: "download",
-    use: '.img2 car',
+    react: "👨‍💻",
+    desc: "Check bot online or no.",
+    category: "main",
+    use: '.alive',
     filename: __filename
 },
-async(conn, mek, m,{from, l, quoted, prefix, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let dat = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
- `
-
-	 const sections = [
-    {
-	title: "",
-	rows: [
-	    
-	    {title: "1", rowId: prefix + 'apk1 ' + q , description: '📥 Download your apk'},
-            {title: "2", rowId: prefix + 'apkinfo ' + q , description: '📃 See apk info'} , 
-
-	]
-    } 
-]
-	const listMessage = {
- text : dat ,
-  footer: config.FOOTER,
-  buttonText: "🔢 Reply below number,",
-  sections,
-  contextInfo: {
-				
-				externalAdReply: { 
-					title: '🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚',
-					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
-					mediaType: 1,
-					sourceUrl: "" ,
-          thumbnailUrl: 'https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg' ,
-					renderLargerThumbnail: false,
-          showAdAttribution: true
-         }}	
-}
- 
-return await conn.replyList(from, listMessage ,{ quoted : mek }) 
-} catch (e) {
-reply(N_FOUND)
-l(e)
-}
-})
-
-
-cmd({
-    pattern: "apkinfo",
-    use: '.song lelena',
-    react: "🎧",
-    desc: descs,
-    category: "download",
-    filename: __filename
-
-},
-
 async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!q) return await reply(imgmsg)
-if(isUrl(q) && !ytreg(q)) return await reply(imgmsg)
-if(isUrl(q) && q.includes('/shorts')){let dat = `┌───[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]`
-
-const sections = [
+	var msg = mek
+if(os.hostname().length == 12 ) hostname = 'replit'
+else if(os.hostname().length == 36) hostname = 'heroku'
+else if(os.hostname().length == 8) hostname = 'koyeb'
+else hostname = os.hostname()
+let monspace ='```'
+let monspacenew ='`'
+if(config.ALIVE === "default") {
+ const sections = [
     {
 	title: "",
 	rows: [
-	    {title: "1", rowId: prefix + 'ytdocs ' + q , description: 'Down song document'},
-	    {title: "2", rowId: prefix + 'ytmp3 ' + q , description: 'Down song audio'} ,
-            {title: "3", rowId: prefix + 'ytinfo ' + q , description: 'To see song info'} ,
+	    {title: "1", rowId: prefix + 'apk1' , description: 'ᴅᴏᴡɴʟᴏᴀᴅ ᴀᴘᴋ'},
+	    {title: "2", rowId: prefix + 'apkinfo' , description: 'ꜱᴇᴇ ᴀᴘᴋ ɪɴꜰᴏ'},
+
 	]
     } 
 ]
 const listMessage = {
-  text: dat,
+  caption: `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
+
+   *QUEEN IZUMI WHATSAPP USER BOT* 💫`,
+  image : { url : config.LOGO} ,
   footer: config.FOOTER,
   buttonText: "🔢 Reply below number,",
   sections,
@@ -123,61 +56,22 @@ const listMessage = {
          }}	
 }
 
-return await conn.replyList(from, listMessage ,{ quoted : mek }) 				      
+return await conn.replyList(from, listMessage ,{ quoted : msg }) 
 }
-if(ytreg(q)){let dat = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
-
-`
-const sections = [
+else {
+  const sections = [
     {
 	title: "",
 	rows: [
-	    {title: "1", rowId: prefix + 'ytdocs ' + q , description: 'Down song document'},
-	    {title: "2", rowId: prefix + 'ytmp3 ' + q , description: 'Down song audio'} ,
-            {title: "3", rowId: prefix + 'ytinfo ' + q , description: 'To see song info'} ,
+	    {title: "1", rowId: prefix + 'apk1' , description: 'ᴅᴏᴡɴʟᴏᴀᴅ ᴀᴘᴋ'},
+	    {title: "2", rowId: prefix + 'apkinfo' , description: 'ꜱᴇᴇ ᴀᴘᴋ ɪɴꜰᴏ'} ,
 
 	]
     } 
 ]
 const listMessage = {
-  text: dat,
-  footer: config.FOOTER,
-  buttonText: "🔢 Reply below number,",
-  sections }	
-
-	     
-return await conn.replyList(from, listMessage ,{ quoted : mek }) 
-
-}
-let yts = require("yt-search")
-let search = await yts(q)
-let anu = search.videos[0]
-const cap = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
-
-   *APK I FORMATION*
-
-*📚 Name :* ${data.name}
-├──────────────────        
-*📦 Developer :* ${data.package}
-├──────────────────        
-*⬆️ Last update :* ${data.lastup}
-├──────────────────        
-*📥 Size :* ${data.size}`
-
-const sections = [
-    {
-	title: "",
-	rows: [
-	    {title: "1", rowId: prefix + 'ytdocs ' + anu.url , description: 'Down song document'},
-	    {title: "2", rowId: prefix + 'ytmp3 ' + anu.url , description: 'Down song audio'} ,
-            {title: "3", rowId: prefix + 'ytinfo ' + anu.url , description: 'To see song info'} ,
-
-	]
-    } 
-]
-const listMessage = {
-  image: {url: anu.thumbnail},
-  caption: cap,
+  caption: config.ALIVE,
+  image : { url : config.LOGO} ,
   footer: config.FOOTER,
   buttonText: "🔢 Reply below number,",
   sections,
@@ -194,10 +88,10 @@ const listMessage = {
          }}	
 }
 
-await conn.replyList(from, listMessage ,{ quoted : mek }) 
-
+return await conn.replyList(from, listMessage ,{ quoted : msg })
+}
 } catch (e) {
-  reply(N_FOUND)
-  l(e)
+reply('*Error !!*')
+l(e)
 }
 })
