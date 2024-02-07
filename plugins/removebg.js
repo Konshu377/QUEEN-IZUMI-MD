944e0d83b9dfdf66924198724b5f26130bc7f25f
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-07 15:23:36 +0530

    Add files via upload

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -57,23 +57,18 @@ let dat = `[🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚]
    *🌆 BACKGROUND REMOVER*
 
 `
-const sections = [
-    {
-	title: "",
-	rows: [
-{title: "1", rowId: prefix + 'rbgi ' + namePng + ".png", description: 'image'},
-{title: "2", rowId: prefix + 'rebgs ' + namePng + ".png", description: 'sticker'},
-{title: "3", rowId: prefix + 'rbgd ' + namePng + ".png", description: 'document'},
+const buttons = [
+{buttonId: prefix + 'rbgi ' + namePng + ".png", buttonText: {displayText: 'IMAGE'}, type: 1},
+{buttonId: prefix + 'rebgs ' + namePng + ".png", buttonText: {displayText: 'STICKER'}, type: 1},
+{buttonId: prefix + 'rbgd ' + namePng + ".png", buttonText: {displayText: 'DOCUMENT'}, type: 1}
 ]
-}
-]
-    const listMessage = {
+    const buttonMessage = {
         caption: dat,
         footer: config.FOOTER,
         buttons: buttons,
         headerType: 1
     }
-    await conn.replyList(from, listMessage,{quoted: mek})
+    return await conn.buttonMessage(from, buttonMessage, mek)
 
 }else return await  reply(imgmsg)
 } catch (e) {
