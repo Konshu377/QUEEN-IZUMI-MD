6cb495fc8bdb172afcc94deeacfb8ed1f1845df5
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-08 10:05:31 +0530

    🧚 ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ 🧚

--- config.js
+++ config.js
@@ -6,16 +6,5 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
-POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
-LANG: process.env.BOT_LANG || 'EN' ,
-PREFIX: process.env.PREFIX || '.',
-ANTI_BAD: process.env.ANTI_BAD || 'false',
-MAX_SIZE: process.env.MAX_SIZE || 300,
-ONLY_GROUP: process.env.ONLY_GROUP || 'false',
-ANTI_LINK: process.env.ANTI_LINK || 'false' ,
-ANTI_BOT: process.env.ANTI_BOT || 'false',
-ALIVE: process.env.ALIVE || `default`,
-FOOTER: process.env.FOOTER ||  '©ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ',
-LOGO: process.env.LOGO || `https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg` 
+SESSION_ID: 'ZEROTWO=kmsRFKBD#CSMW_N13TIMTA3i5fUHIoXqX7XGrNUKCgxtz34QmI2M'
 };
